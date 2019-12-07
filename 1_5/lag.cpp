f57edd748795f2e59f9632f4c4682a4c2be0013b
vaipatel <vai.pateln@gmail.com>
2019-12-07 01:29:18 -0500

    Add sln to 1.5 using simple helper with strict pre
    Makes the helper shorter and caller of helper easier to understand, imo.

---
+++ 1_5/lag.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// PRE: 
+// 1) 0 <= s.length()
+// 2) s.length() == b.length
+// 3) s.length() == b.length() - 1
+bool oneEdit(string s, string b)
+{
+    int lag = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (s[i - lag] != b[i])
+        {
+            if (lag == 1) return false;
+            
+            lag = 1;
+        }
+    }
+    return true;
+}
+
+bool checkOneEdit(string s1, string s2)
+{
+    int s1Len = s1.length(), s2Len = s2.length();
+    if (s1Len == s2Len)
+    {
+        return oneEdit(s1, s2);
+    }
+    if (s1Len == s2Len - 1)
+    {
+        return oneEdit(s1, s2);
+    }
+    if (s2Len == s1Len - 1)
+    {
+        return oneEdit(s2, s1);
+    }
+    return false;
+}
+
+int main()
+{
+    cout << checkOneEdit("appee", "apple") << endl;
+    cout << checkOneEdit("", "") << endl;
+    cout << checkOneEdit("a", "") << endl;
+    cout << checkOneEdit("", "x") << endl;
+    cout << checkOneEdit("latte", "late") << endl;
+    cout << checkOneEdit("lass", "lasso") << endl;
+    cout << !checkOneEdit("hello", "helen") << endl;
+    cout << !checkOneEdit("apppl", "apl") << endl;
+    return 0;
+}
