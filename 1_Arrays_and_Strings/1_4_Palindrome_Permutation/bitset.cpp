18ae2d1b37f291855fc349cf86ec86ce9850e2a7
vaipatel <vai.pateln@gmail.com>
2019-12-07 01:46:36 -0500

    Reorganizize

---
+++ 1_Arrays_and_Strings/1_4_Palindrome_Permutation/bitset.cpp
@@ -0,0 +1,27 @@
+#include <iostream> // cout
+#include <cctype> // tolower
+#include <cstring> // strlen
+#include <bitset> // bitset
+
+bool isPermOfPalindrome(const char *s)
+{
+    int l = strlen(s);
+    unsigned int b = 0;
+    for (int i = 0; i < l; ++i)
+    {
+        // get the int for s[i]
+        int c = tolower(s[i]) - 'a';
+        // ignore non-letter chars
+        if (c < 0) continue;
+        // just retoggle the bits for c using XOR
+        b = b ^ (1 << c);
+    }
+    // b should have just one or no bits set to be a palindrome perm.
+    std::bitset<26> bits(b);
+    return (b == 0) || (bits.count() == 1);
+}
+
+int main()
+{
+  std::cout << isPermOfPalindrome("caac") << std::endl;
+}
